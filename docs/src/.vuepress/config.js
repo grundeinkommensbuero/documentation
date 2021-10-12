27b12a6520b2ebb775b70b87df93a37102ed3e4e
nmauersberg <n.mauersberg@gmail.com>
2021-10-12 11:06:27 +0200

    Add basic VuePress configuration

---
+++ docs/src/.vuepress/config.js
@@ -0,0 +1,68 @@
+const { description } = require('../../package');
+
+module.exports = {
+  /**
+   * Ref：https://v1.vuepress.vuejs.org/config/#title
+   */
+  title: 'XBGE documentation',
+  /**
+   * Ref：https://v1.vuepress.vuejs.org/config/#description
+   */
+  description: description,
+
+  /**
+   * Extra tags to be injected to the page HTML `<head>`
+   *
+   * ref：https://v1.vuepress.vuejs.org/config/#head
+   */
+  head: [
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    [
+      'meta',
+      { name: 'apple-mobile-web-app-status-bar-style', content: 'black' },
+    ],
+  ],
+
+  /**
+   * Theme configuration, here is the default theme configuration for VuePress.
+   *
+   * ref：https://v1.vuepress.vuejs.org/theme/default-theme-config.html
+   */
+  themeConfig: {
+    repo: '',
+    editLinks: false,
+    docsDir: '',
+    editLinkText: '',
+    lastUpdated: false,
+    nav: [
+      {
+        text: 'Overview',
+        link: '/overview/',
+      },
+      {
+        text: 'Website',
+        link: 'https://expedition-grundeinkommen.de',
+      },
+    ],
+    sidebar: {
+      '/overview/': [
+        {
+          title: 'Frontend',
+          collapsable: false,
+          children: ['frontend'],
+        },
+        {
+          title: 'Backend',
+          collapsable: false,
+          children: ['backend'],
+        },
+      ],
+    },
+  },
+
+  /**
+   * Apply plugins，ref：https://v1.vuepress.vuejs.org/zh/plugin/
+   */
+  plugins: ['@vuepress/plugin-back-to-top', '@vuepress/plugin-medium-zoom'],
+};
---
+++ docs/src/index.md
@@ -0,0 +1,15 @@
+---
+home: true
+heroImage: https://images.ctfassets.net/af08tobnb0cl/AZm2hUCdZyiX6p1nDEC81/d47a4f383ac61c641c57f64d9c189dba/xbge-logo.svg
+tagline: A markdown renderer for the technical documentation of our website
+actionText: Overview →
+actionLink: /overview/
+# features:
+#   - title: Feature 1 Title
+#     details: Feature 1 Description
+#   - title: Feature 2 Title
+#     details: Feature 2 Description
+#   - title: Feature 3 Title
+#     details: Feature 3 Description
+footer: Made by  with ❤️
+---
